34b488e6a56dc4cd6e8cd79c6eeb6ca8986b8dd6
nykotar <atrevales@protonmail.com>
2020-10-27 16:51:18 -0300

    add travis ci

--- rvme/settings/tests.py
+++ rvme/settings/tests.py
@@ -3,9 +3,8 @@ from .local import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'github_actions',
+        'NAME': 'travis',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }

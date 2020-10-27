178a3d3bd7ae96c8ca621b655b9693e62ae1f319
nykotar <atrevales@protonmail.com>
2020-10-27 19:34:21 -0300

    Test practice views

--- pool/tests/factories.py
+++ pool/tests/factories.py
@@ -2,6 +2,8 @@ import factory
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
+from ..models import PoolTarget
+
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
@@ -12,4 +14,15 @@ class UserFactory(factory.django.DjangoModelFactory):
     def password(self, create, extracted, **kwargs):
         self.set_password('test123')
         self.save()
+
+class TargetFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = PoolTarget
     
+    category = 'OTHER'
+    feedback_img = factory.django.ImageField(color='blue')
+    feedback_img_phash = 'aaaaaaaaaaaaaaaa'
+    feedback_img_chash = 'aaaaaaaaaaaaaaaa'
+    tasking = 'test'
+    target_description = 'test'
+    active = True
--- pool/tests/test_views.py
+++ pool/tests/test_views.py
@@ -3,11 +3,13 @@ from django.test import TestCase
 from django.urls import reverse
 
 from . import factories
+from ..models import Target
 
 class TestPracticeViews(TestCase):
 
     def setUp(self):
         self.user = factories.UserFactory()
+        self.practice_url = reverse('pool:index')
 
     def test_anonymous_user_cant_get_target(self):
         practice_url = reverse('pool:index')
@@ -18,6 +20,41 @@ class TestPracticeViews(TestCase):
     
     def test_logged_user_can_get_target(self):
         self.client.login(username=self.user.username, password='test123')
-        practice_url = reverse('pool:index')
-        response = self.client.post(practice_url, {'event':True}, follow=True)
+        response = self.client.post(self.practice_url, {'event':True}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_get_and_reveal_target(self):
+        factories.TargetFactory()
+        self.client.login(username=self.user.username, password='test123')
+        response = self.client.post(self.practice_url, {'other':True}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        target_id = response.redirect_chain[0][0][-9:]
+        reveal_url = reverse('pool:reveal_target', kwargs={'tid': target_id})
+        response = self.client.get(reveal_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        target = Target.objects.get(user=self.user, target_id=target_id)
+        self.assertTrue(target.revealed)
+    
+    def test_can_get_and_reveal_precog_target(self):
+        factories.TargetFactory()
+        self.client.login(username=self.user.username, password='test123')
+        response = self.client.post(self.practice_url, {'other':True, 'precognitive':True}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        target_id = response.redirect_chain[0][0][-9:]
+        target = Target.objects.get(user=self.user, target_id=target_id)
+        self.assertIsNone(target.pool_target, msg='A pooltarget was assigned to a precognitive target ahead of time!')
+        reveal_url = reverse('pool:reveal_target', kwargs={'tid': target_id})
+        response = self.client.get(reveal_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        target = Target.objects.get(user=self.user, target_id=target_id)
+        self.assertTrue(target.revealed)
+
+    def test_can_access_target_public_url(self):
+        factories.TargetFactory()
+        self.client.login(username=self.user.username, password='test123')
+        response = self.client.post(self.practice_url, {'other':True}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        target_id = response.redirect_chain[0][0][-9:]
+        target = Target.objects.get(user=self.user, target_id=target_id)
+        response = self.client.get(reverse('pool:shared_target_detail', kwargs={'uuid':target.target_uid}))
         self.assertEqual(response.status_code, 200)

6f8cf1150fb4ee9ee7c6f7069edb79dec0b764f1
nykotar <atrevales@protonmail.com>
2020-12-31 16:14:56 -0300

    Implementing levels and removing categories

--- pool/forms.py
+++ pool/forms.py
@@ -5,15 +5,12 @@ from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV2Checkbox
 
 class UploadTargetForm(forms.Form):
-    categories = (
-        ('PERSON', 'Person'),
-        ('LIFEFORM', 'Lifeform'),
-        ('OBJECT', 'Object'),
-        ('LOCATION', 'Location'),
-        ('EVENT', 'Event'),
-        ('OTHER', 'Other')
+    LEVELS = (
+        ('BEGINNER', 'Beginner'),
+        ('INTERMEDIATE', ' Intermediate'),
+        ('ADVANCED', 'Advanced')
     )
-    category = forms.ChoiceField(choices=categories)
+    level = forms.ChoiceField(choices=LEVELS, widget=forms.RadioSelect)
     additional_feedback = forms.CharField(widget=forms.Textarea(), required=False)
     tasking = forms.CharField(widget=forms.Textarea())
     target_description = forms.CharField(max_length=255)
@@ -21,30 +18,30 @@ class UploadTargetForm(forms.Form):
     captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
 
 class GetTargetForm(forms.Form):
-    person = forms.BooleanField(required=False)
-    lifeform = forms.BooleanField(required=False)
-    object = forms.BooleanField(required=False)
-    location = forms.BooleanField(required=False)
-    event = forms.BooleanField(required=False)
-    other = forms.BooleanField(required=False)
+    LEVELS = (
+        ('BEGINNER', 'Beginner'),
+        ('INTERMEDIATE', ' Intermediate'),
+        ('ADVANCED', 'Advanced')
+    )
+    level = forms.ChoiceField(choices=LEVELS, widget=forms.RadioSelect)
     precognitive = forms.BooleanField(required=False)
     incsubmitted = forms.BooleanField(required=False)
 
-    def clean(self):
-        cleaned_data = super().clean()
+    # def clean(self):
+    #     cleaned_data = super().clean()
 
-        categories = ['person', 'lifeform', 'object',
-                      'location', 'event', 'other']
-        selected_categories = list()
-        for category in categories:
-            if cleaned_data.get(category):
-                selected_categories.append(category.upper())
+    #     categories = ['person', 'lifeform', 'object',
+    #                   'location', 'event', 'other']
+    #     selected_categories = list()
+    #     for category in categories:
+    #         if cleaned_data.get(category):
+    #             selected_categories.append(category.upper())
 
-        if not selected_categories:
-            raise ValidationError('You need to select at least one category.')
+    #     if not selected_categories:
+    #         raise ValidationError('You need to select at least one category.')
 
-        cleaned_data['selected_categories'] = selected_categories
-        return cleaned_data
+    #     cleaned_data['selected_categories'] = selected_categories
+    #     return cleaned_data
 
 class NewPersonalTargetForm(forms.Form):
     tasking = forms.CharField(widget=forms.Textarea(), required=True)

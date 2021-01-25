6cc7e53cc7ef7e788be858860d9f036d26354999
nykotar <atrevales@protonmail.com>
2021-01-25 01:54:06 -0300

    Dont let precog mode get targets the user already done, unless no target is available

--- pool/views.py
+++ pool/views.py
@@ -108,10 +108,16 @@ class RevealTargetView(LoginRequiredMixin, View):
         target = get_object_or_404(Target, target_id=tid, user=request.user)
 
         if target.is_precog:
-            available_targets = PoolTarget.objects.filter(level=target.level, active=True)
+            available_targets_ = PoolTarget.objects.filter(level=target.level, active=True)
             if not target.inc_submitted:
-                available_targets = available_targets.exclude(submission__submitted_by=request.user)
+                available_targets_ = available_targets_.exclude(submission__submitted_by=request.user)
+            done_targets = Target.objects.filter(pool_target__level=target.level,
+                                                user=self.request.user).values_list('pool_target', flat=True)
+            available_targets = available_targets_.exclude(pk__in=done_targets)
             count = available_targets.count()
+            if count == 0:
+                available_targets = available_targets_
+                count = available_targets.count()
             sel_target = available_targets[randint(0, count - 1)]
             target.pool_target = sel_target
 
